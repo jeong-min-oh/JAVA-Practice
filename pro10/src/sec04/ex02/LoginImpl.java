fbc07dedcda6193596f373093b46c123213d0497
jeong-min-oh <63306450+jeong-min-oh@users.noreply.github.com>
2022-12-20 16:55:49 +0900

    Add files via upload

---
+++ pro10/src/sec04/ex02/LoginImpl.java
@@ -0,0 +1,49 @@
+package sec04.ex02;
+
+import javax.servlet.annotation.WebListener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class LoginImpl
+ *
+ */
+@WebListener
+public class LoginImpl implements HttpSessionListener {
+	
+	String user_id;
+	String user_pw;
+	static int total_user=0;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginImpl() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public LoginImpl(String user_id, String user_pw) {
+    	this.user_id = user_id;
+    	this.user_pw = user_pw;
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    @Override
+    public void sessionCreated(HttpSessionEvent se)  { 
+         System.out.println("세션 생성");
+         ++total_user;
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+        System.out.println("세션 소멸");
+        --total_user;
+    }
+	
+}
