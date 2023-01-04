b304b9d292437b28fc9b9f29aa35166c8bf70a49
jeong-min-oh <63306450+jeong-min-oh@users.noreply.github.com>
2023-01-04 18:15:18 +0900

    영화 예매 시스템 View

---
+++ view/CinemaView.java
@@ -0,0 +1,24 @@
+package com.java.view;
+
+import java.util.Map;
+
+import com.java.dto.CinemaVO;
+
+public class CinemaView {
+
+	public Map<String, String> CinemaSearch() {
+		
+		return null;
+	}
+
+	public Map<String, String> CinemaRegist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<String, CinemaVO> CinemaModify() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
