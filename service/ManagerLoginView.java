5b4af259943ab8af2c7b17de04ad18eda3e3152e
jeong-min-oh <63306450+jeong-min-oh@users.noreply.github.com>
2023-01-04 18:12:31 +0900

    영화 예매시스템 Service, ServiceImpl

---
+++ service/ManagerLoginView.java
@@ -0,0 +1,18 @@
+package com.java.service;
+
+import java.util.Map;
+
+public class ManagerLoginView {
+
+	public Map<String, String> ManagerLogin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+ void list() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
